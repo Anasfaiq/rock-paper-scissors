7e08d4af31d603d0359e908db84cc7cdfcaae46f
Anas Faiq <176678988+Anasfaiq@users.noreply.github.com>
2025-10-23 13:11:08 +0700

    semua fungsi js nya sudah selesai

--- script.js
+++ script.js
@@ -10,45 +10,90 @@ function getComputerChoice(result) {
   return result;
 }
 
-function getHumanChoice(result) {
-  result = prompt("What's your choice. Rock, Paper, Scissors?");
-  return result;
-}
+// udah gak di pake karna sekarang udah ada GUI nya
+// function getHumanChoice(result) {
+//   result = prompt("What's your choice. Rock, Paper, Scissors?");
+//   return result;
+// }
 
 let humanScore = 0;
 let computerScore = 0;
 
+
 function playRound (humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
   computerChoice = computerChoice.toLowerCase();
-
+  
   if (humanChoice === computerChoice) {
     humanScore += 1;
     computerScore += 1;
-    console.log(`Draw! The current score is ${humanScore} - ${computerScore}`);
+    div.textContent = `Draw! The current score is ${humanScore} - ${computerScore}`;
+    // console.log(`Draw! The current score is ${humanScore} - ${computerScore}`);
   } else if (humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "rock" && computerChoice === "scissors") {
     humanScore += 1;
-    console.log(`You Win! The current score is ${humanScore} - ${computerScore}`);
+    div.textContent = `You Win! The current score is ${humanScore} - ${computerScore}`;
+    // console.log(`You Win! The current score is ${humanScore} - ${computerScore}`);
   } else if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
     computerScore += 1;
-    console.log(`You Lose! The current score is ${humanScore} - ${computerScore}`);
+    div.textContent = `You Lose! The current score is ${humanScore} - ${computerScore}`
+    // console.log(`You Lose! The current score is ${humanScore} - ${computerScore}`);
   }
+  
+  if (humanScore === 5 || computerScore === 5){
+    if (humanScore === 5){
+      div.textContent = `You won the game! the final score is ${humanScore} - ${computerScore}`
+      // console.log("You won the game!");
+    } else {
+      div.textContent = `You lose, the final score is ${humanScore} - ${computerScore}`
+      // console.log("You lose");
+    }
+    // untuk disable button agar tidak bisa lanjut saat salah satu score sudah mencapai 5
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+  }
+  
 }
 
-function playGame() {
-  for (let i = 1; i <= 5; i++ ) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection)
-  }
+//   // udah gak dipake lagi
 
-  if (humanScore > computerScore) {
-    console.log(`You Win! The final score is ${humanScore} - ${computerScore}`);
-  } else if (humanScore < computerScore) {
-    console.log(`You Lose! The final score is ${humanScore} - ${computerScore}`);
-  } else if (humanScore  === computerScore) {
-    console.log(`Draw! The final score is ${humanScore} - ${computerScore}`);
-  }
-}
+// function playGame() {
+  
+//   // for (let i = 1; i <= 5; i++ ) {
+  //   //   let humanSelection = getHumanChoice();
+  //   //   let computerSelection = getComputerChoice();
+  //   //   playRound(humanSelection, computerSelection)
+  //   // }
+  
+  //   if (humanScore > computerScore) {
+    //     console.log(`You Win! The final score is ${humanScore} - ${computerScore}`);
+    //   } else if (humanScore < computerScore) {
+      //     console.log(`You Lose! The final score is ${humanScore} - ${computerScore}`);
+      //   } else if (humanScore  === computerScore) {
+        //     console.log(`Draw! The final score is ${humanScore} - ${computerScore}`);
+        //   }
+        // }
 
-playGame();
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const restartBtn = document.querySelector("#restartBtn");
+const div = document.querySelector("#score");
+
+rockBtn.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paperBtn.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener('click', () => playRound("scissors", getComputerChoice()));
+
+        
+        
+// bagian ini berguna untuk me restart score game
+restartBtn.addEventListener('click', () => {
+  humanScore = 0;
+  computerScore = 0;
+  alert("Game restarted! Score reset to 0 - 0")
+  div.textContent = "";
+
+  rockBtn.disabled = false;
+  paperBtn.disabled = false;
+  scissorsBtn.disabled = false;
+})
